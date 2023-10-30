19699f1a977fa534158137e4bb258483dea685f7
Asrat <aabebe.navy@gmail.com>
2023-10-30 00:49:16 -0700

    week 4

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,18 +15,61 @@
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
+describe("shuffle", () => {
+    it("takes in an array, removes the first item from the array, and shuffles the remaining items", () => {
+      expect(shuffle(["purple", "blue", "green", "yellow", "pink"])).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+      expect(shuffle(["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"])).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
+    })
+  })
+
+
 const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+
+
 const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+
+
 // b) Create the function that makes the test pass.
 
+const shuffle = (arr) => {
+    if (arr.length > 0) {
+      const firstItem = arr.shift()
+      
+      for (let i = arr.length - 1; i > 0; i--) {
+        const shuff = Math.floor(Math.random() * (i + 1)) 
+        [arr[i], arr[shuff]] = [arr[shuff], arr[i]]
+      }
+      
+      arr.unshift(firstItem)
+    }
+    
+    return arr
+  }
+  
+
 // Pseudo code:
+// Define a function to shuffle an array 
+// Remove the first item from the array firstItem = array.shift(
+// Iterate over the remaining items and shuffle them with for loop 
+// Generate a random index within the range [0, i]
+// Swap the current item with the randomly selected item
+// Add the first item back to the shuffled array// array.unshift(firstItem)
+// Return the shuffled array
+
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("calculateTally", () => {
+    it("calculates the end tally for different vote counts", () => {
+      expect(calculateTally({ upVotes: 13, downVotes: 2 })).toEqual(11)
+      expect(calculateTally({ upVotes: 2, downVotes: 33 })).toEqual(-31)
+    })
+  })
 
 const votes1 = { upVotes: 13, downVotes: 2 }
 // Expected output: 11
@@ -35,4 +78,13 @@ const votes2 = { upVotes: 2, downVotes: 33 }
 
 // b) Create the function that makes the test pass.
 
+function calculateTally(votes) {
+    const { upVotes, downVotes } = votes
+    const tally = upVotes - downVotes
+    return tally
+  }
 // Pseudo code:
+// Define a function to calculate the end tally function - calculateTally
+// Extract the number of up votes and down votes from the input object - upVotes = votes.upVotes
+// Calculate the tally by subtracting down votes from up votes tally = upVotes - downVotes
+// Return the calculated tally return tally
